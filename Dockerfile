4fa403012937615bb01ccabb92ba5f7fa274a2ba
Ubuntu <ubuntu@ip-172-31-32-201.ap-south-1.compute.internal>
2025-05-21 18:22:53 +0000

    added doc type with files(ask q&a)

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.10-slim
+
+WORKDIR /app
+
+COPY . /app
+
+RUN apt-get update && apt-get install -y \
+    python3-dev \
+    libpq-dev \
+    gcc \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+RUN  pip install --upgrade wheel
+RUN  pip install --upgrade setuptools
+RUN  pip install --upgrade pip
+RUN  pip install -r requirements.txt
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- services/api_service.py
+++ services/api_service.py
@@ -33,16 +33,17 @@ async def upload_files_conversation(files, doc_type, user_id):
 
     # Process uploaded files
     newly_uploaded = []
+    file_details = []
     for uploaded_file in files:
         if not uploaded_file.filename.lower().endswith('.pdf'):
             continue
             
         file_name = uploaded_file.filename
-        file_path = os.path.join(f"{UPLOAD_DIR}\{user_id}", file_name)
+        file_path = os.path.join(f"{UPLOAD_DIR}/{user_id}", file_name)
         # file_key = f"{doc_type}_{file_name}"
 
         # Record upload in database
-        db_op.document_upload_info(file_name, user_id)
+        file_id = db_op.document_upload_info(file_name, user_id, doc_type)
 
         # Create the directory structure if it doesn't exist
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
@@ -51,15 +52,16 @@ async def upload_files_conversation(files, doc_type, user_id):
             await uploaded_file.seek(0)
             with open(file_path, "wb") as f:
                 f.write(await uploaded_file.read())
-            newly_uploaded.append(file_name)
+            newly_uploaded.append({"file_path": file_path, "file_id": file_id})
+            file_details.append({"file_name": file_name, "file_id": file_id})
 
     if newly_uploaded:
-        for file_name in newly_uploaded:
-            pdf_reader.create_embeddings(filename=file_path)
+        for file_var in newly_uploaded:
+            pdf_reader.create_embeddings(filename=file_var["file_path"], file_id=file_var["file_id"])
     
     return {
-        "message": f"Successfully uploaded and processed {len(newly_uploaded)} files.",
-        "uploaded_files": newly_uploaded
+        "message": f"Successfully uploaded and processed {len(file_details)} files.",
+        "uploaded_files": file_details
     }
 
 
@@ -79,13 +81,19 @@ async def manage_category(user_id):
     return data
 
 async def conversations(request):
-
+    
     # Get answer
     response = inference_obj.get_answer(
         request.prompt, 
         selected_doc_type=request.doc_type, 
-        user_id=request.user_id
+        user_id=request.user_id,
+        file_ids = request.file_ids
     )
 
     return response
 
+async def file_details_by_doc_type(user_id, doc_type):
+
+    data = db_op.extract_doc_upload_table_data(user_id, doc_type)
+
+    return data
