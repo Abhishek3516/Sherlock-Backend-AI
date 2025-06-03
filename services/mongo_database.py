cea6c1d73ece168b39fdc13e0c647c760757be73
Abhishek <abhihappy3516@gmail.com>
2025-06-04 03:26:28 +0530

    mongodb url placement

--- services/mongo_database.py
+++ services/mongo_database.py
@@ -1,8 +1,12 @@
 from pymongo import MongoClient
 import urllib.parse
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-MONGODB_URL = "mongodb+srv://ksatyam1729:s8xaHOnUQevmyq1k@ravian-cluster.ulapr.mongodb.net/sherlock-db-uat?retryWrites=true&w=majority"
-mongodb_uri = f"{MONGODB_URL}&ssl=true&ssl_cert_reqs=CERT_NONE"
+
+mongodb_uri = f"{os.getenv("MONGODB_URL")}&ssl=true&ssl_cert_reqs=CERT_NONE"
+
 port = 8000
 client = MongoClient(mongodb_uri, port)
 db = client["sherlock-db-uat"]
